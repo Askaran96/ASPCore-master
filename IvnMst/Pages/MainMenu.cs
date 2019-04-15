7f592699abe892dffa3c0474411589ec04c0dc73
Askaran <askarandeep@gmail.com>
2019-04-16 09:48:13 +1200

    Add project files.

---
+++ IvnMst/Pages/MainMenu.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InvMst.Pages
+{
+    public static class MainMenu
+    {
+        public static class Customer
+        {
+            public const string PageName = "Customer";
+            public const string RoleName = "Customer";
+            public const string Path = "/Customer/Index";
+            public const string ControllerName = "Customer";
+            public const string ActionName = "Index";
+        }
+
+        public static class Vendor
+        {
+            public const string PageName = "Vendor";
+            public const string RoleName = "Vendor";
+            public const string Path = "/Vendor/Index";
+            public const string ControllerName = "Vendor";
+            public const string ActionName = "Index";
+        }
+
+        public static class CustomerType
+        {
+            public const string PageName = "Customer Type";
+            public const string RoleName = "Customer Type";
+            public const string Path = "/CustomerType/Index";
+            public const string ControllerName = "CustomerType";
+            public const string ActionName = "Index";
+        }
+
+        public static class VendorType
+        {
+            public const string PageName = "Vendor Type";
+            public const string RoleName = "Vendor Type";
+            public const string Path = "/VendorType/Index";
+            public const string ControllerName = "VendorType";
+            public const string ActionName = "Index";
+        }
+
+        public static class User
+        {
+            public const string PageName = "User";
+            public const string RoleName = "User";
+            public const string Path = "/UserRole/Index";
+            public const string ControllerName = "UserRole";
+            public const string ActionName = "Index";
+        }
+
+        public static class ChangePassword
+        {
+            public const string PageName = "Change Password";
+            public const string RoleName = "Change Password";
+            public const string Path = "/UserRole/ChangePassword";
+            public const string ControllerName = "UserRole";
+            public const string ActionName = "ChangePassword";
+        }
+
+        public static class Role
+        {
+            public const string PageName = "Role";
+            public const string RoleName = "Role";
+            public const string Path = "/UserRole/Role";
+            public const string ControllerName = "UserRole";
+            public const string ActionName = "Role";
+        }
+
+        public static class ChangeRole
+        {
+            public const string PageName = "Change Role";
+            public const string RoleName = "Change Role";
+            public const string Path = "/UserRole/ChangeRole";
+            public const string ControllerName = "UserRole";
+            public const string ActionName = "ChangeRole";
+        }
+
+        public static class Dashboard
+        {
+            public const string PageName = "Dashboard Main";
+            public const string RoleName = "Dashboard Main";
+            public const string Path = "/Dashboard/Index";
+            public const string ControllerName = "Dashboard";
+            public const string ActionName = "Index";
+        }
+
+    }
+}
